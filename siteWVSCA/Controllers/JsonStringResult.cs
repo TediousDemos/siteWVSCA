3543361bd31068d0f540af9d6845848c240993eb
TediousDemos <jackatimmons@gmail.com>
2016-06-28 14:02:29 -0400

    test

---
+++ siteWVSCA/Controllers/JsonStringResult.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace siteWVSCA.Controllers
+{
+    public class JsonStringResult : ContentResult
+    {
+        // GET: JSON
+        public JsonResult fnname()
+        {
+            string variablename = "{name:'jack',message:'success'}";
+            return Json(variablename, JsonRequestBehavior.AllowGet);
+        }
+
+        private JsonResult Json(string variablename, JsonRequestBehavior allowGet)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
